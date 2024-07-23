c02df77d048b4ef096741ec4e4f0dd6b01daf33a
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-07-23 22:28:36 +0200

    sidebar always open

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
           initial: "Hi! 👋 I'm here to help you get stuff done.",
         }}
         defaultOpen={true}
+        clickOutsideToClose={false}
       >
         <TasksProvider>
           <TasksList />
