963476ac59149280d83aab90562352de5bfd3de6
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-07-24 12:09:11 +0200

    remove copilotkit style import

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import { TasksList } from "@/components/TasksList";
 import { TasksProvider } from "@/lib/hooks/use-tasks";
-import "@copilotkit/react-ui/styles.css";
 
 export default function Home() {
   return (
