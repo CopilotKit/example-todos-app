caadbafbc4be9256b32d04c6ae7431c0257b85b4
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-07-24 12:08:05 +0200

    remove copilotkit provider

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,12 @@
 
 import { TasksList } from "@/components/TasksList";
 import { TasksProvider } from "@/lib/hooks/use-tasks";
-import { CopilotKit } from "@copilotkit/react-core";
 import "@copilotkit/react-ui/styles.css";
 
 export default function Home() {
   return (
-    <CopilotKit publicApiKey="<your-api-key>">
-      <TasksProvider>
-        <TasksList />
-      </TasksProvider>
-    </CopilotKit>
+    <TasksProvider>
+      <TasksList />
+    </TasksProvider>
   );
 }
