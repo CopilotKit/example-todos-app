9a60b3c9e8bbf15257a233a068e9ea676dbdc0fc
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-07-24 12:00:26 +0200

    add suspense to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <Suspense>{children}</Suspense>
       </body>
     </html>
   );
