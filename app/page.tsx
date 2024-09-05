45875f0788c953c769663dad2295d4eba9fc2bac
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-09-05 11:37:18 +0200

    remove logging

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,6 @@ import "@copilotkit/react-ui/styles.css";
 export default function Home() {
   const COPILOT_CLOUD_PUBLIC_API_KEY = process.env.NEXT_PUBLIC_COPILOT_CLOUD_PUBLIC_API_KEY;
 
-  console.log(COPILOT_CLOUD_PUBLIC_API_KEY);
-
   return (
     <>
       <CopilotKit publicApiKey={COPILOT_CLOUD_PUBLIC_API_KEY}>
