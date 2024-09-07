6e30a4a8b94e023966754159566ddb93c7806194
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-09-07 22:27:22 +0200

    add css

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { TasksList } from "@/components/TasksList";
 import { TasksProvider } from "@/lib/hooks/use-tasks";
 import { CopilotKit } from "@copilotkit-alt/react-core";
 import { CopilotPopup } from "@copilotkit-alt/react-ui";
-import "@copilotkit/react-ui/styles.css";
+import "@copilotkit-alt/react-ui/styles.css";
 
 export default function Home() {
   return (
